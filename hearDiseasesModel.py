4920722a9aa4cef1b6b0c3a346e89012b237c0bf
Akash Ghosh <96739678+akashghosh20@users.noreply.github.com>
2023-10-31 00:36:59 +0600

    Update hearDiseasesModel.py

--- hearDiseasesModel.py
+++ hearDiseasesModel.py
@@ -2,40 +2,42 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import joblib
 
+# Load the data
 data = pd.read_csv("/content/heart.csv")
-
-# Assuming 'sex' is the only categorical feature
-categorical_features = ['sex']
-numeric_features = [col for col in data.columns if col not in ['target'] + categorical_features]
-
-# Create a ColumnTransformer
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', StandardScaler(), numeric_features),
-        ('cat', OneHotEncoder(drop='first'), categorical_features)
-    ])
-
-# Create a pipeline with the preprocessor and the model
-model = Pipeline(steps=[('preprocessor', preprocessor),
-                        ('classifier', LogisticRegression())])
 
 # Split the data
 x = data.drop(columns='target', axis=1)
 y = data['target']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=2)
 
-# # Train the model
-# model.fit(x_train, y_train)
+# Define the transformer
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), x.columns),
+        ('cat', OneHotEncoder(), ['sex', 'fbs', 'exang'])
+    ])
 
-# # Save the preprocessor
-# joblib.dump(preprocessor, 'preprocessor.pkl')
+# Create a pipeline with the preprocessor and the model
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('classifier', LogisticRegression())
+])
 
-# # Save the model
+# Train the logistic regression model
+model.fit(x_train, y_train)
+
+# Predict on the training set
+y_train_pred = model.predict(x_train)
+
+# Calculate accuracy on the training set
+accuracy = accuracy_score(y_train_pred, y_train)
+print(f"Accuracy on the training set: {accuracy}")
+
+# Save the model
 # joblib.dump(model, 'heart_disease_model.pkl')
-x_train.head()
